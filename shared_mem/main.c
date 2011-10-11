80a61278576e111f0372b08b9c613ee7e7910c48
Andrew Marshall <esussma1@binghamton.edu>
2011-10-10 21:09:14 -0400

    Have server and client sending messages to one another

--- shared_mem/main.c
+++ shared_mem/main.c
@@ -5,42 +5,94 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-#define SHM_SIZE 1024  /* make it a 1K shared memory segment */
+ /* make it a 1K shared memory segment */
+#define SHM_SIZE 1024
+
+typedef struct{
+  char message[32];
+  int status;
+} data;
 
 int main(int argc, char ** argv)
 {
-	if(strcmp(argv[1], "s") == 0)
+  char* connectionType;
+  if(argc > 1)
+    connectionType = argv[1];
+  else
+    connectionType = "q";
+
+if(strcmp(connectionType, "s") == 0)
 	{
 		printf("Server!\n");
-		key_t key = ftok("test",'9');
-		size_t size = 1024;
-		int shmid;
-		char *data;
-		
-		if(shmid = shmget(key, size, 0644 | IPC_CREAT | IPC_EXCL) < 0)
+		key_t key = 99;
+    int shmid;
+		data *waitingForMessage;
+
+    if((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT)) < 0)
 		{
 			printf("SHMGET IS WRONG!!!\n");
 			return -1;
 		}
-		
-		shmid = shmget(key, SHM_SIZE, 0644);
-		
+    waitingForMessage = shmat(shmid, (void *)0, 0);
+
+    if (waitingForMessage == (data *)(-1))
+    {
+      perror("shmat");
+      exit(1);
+    }
+
+    while(!waitingForMessage->status)
+    {
+      if(waitingForMessage->status)
+      {
+        printf("In server: %s", waitingForMessage->message);
+        waitingForMessage->status = 1;
+      }
+
+    }
+
 		 /* delete the segment */
        if( shmctl(shmid, IPC_RMID, NULL) == -1) {
             perror("shmctl");
             exit(1);
        }
-		
 	}
-	else if(strcmp(argv[1], "c") == 0)	
+	else if(strcmp(connectionType, "c") == 0)
 	{
 		printf("Client!\n");
-	}
-	
-	
-	
+	  size_t size = 1024;
+    key_t key = 99;
+		int shmid;
+		data *prepareData;
 
-	
+	  if((shmid = shmget(key, size, 0644 | IPC_CREAT)) < 0)
+		{
+			printf("SHMGET IS WRONG!!!\n");
+			return -1;
+		}
+
+    prepareData = shmat(shmid, (void *)0, 0);
+
+    if (prepareData == (data *)(-1))
+    {
+      perror("shmat");
+      exit(1);
+    }
+    char c = ' ';
+    int sizeOfString = 20;
+    int charIndex = 0;
+    while(c != '\n')
+    {
+      c = getchar();
+      prepareData->message[charIndex] = c;
+      charIndex++;
+    }
+    prepareData->status = 1;
+  }
+
+
+
+
 	return 0;
 }
 
